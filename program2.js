5e199f6c18a2af53265207eec0390faf333b48ad
unknown <lodhianurag803@gmail.com>
2024-07-26 12:25:10 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -5,3 +5,21 @@ const decodeTheRing = function (s, p) {
   };
   
   module.exports = decodeTheRing;
+
+
+  // Function to decode the ring based on the pattern
+function decodeTheRing(str, pattern) {
+  if (str.length !== pattern.length) {
+      return false;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+      if (pattern[i] !== '*' && str[i] !== pattern[i]) {
+          return false;
+      }
+  }
+
+  return true;
+}
+
+module.exports = decodeTheRing;
